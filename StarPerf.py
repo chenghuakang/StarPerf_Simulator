f86c4c8ed98c6f21d76f898ec72796544c5717c5
kangchenghua <chenghuakang@gmail.com>
2026-05-15 02:12:30 +0200

    Update StarPerf.py

--- StarPerf.py
+++ StarPerf.py
@@ -12,7 +12,7 @@ def main():
     print("Starting StarPerf...")
 
     import src.constellation_generation.by_XML.constellation_information as constellation_information
-    constellation_information.constellation_information("config/XML_constellation/Iridium.xml)
+    constellation_information.constellation_information("config/XML_constellation/Iridium.xml")
 
     print("Starting XML Constellations Testing...")
     # test the core module functionality of various XML constellations
