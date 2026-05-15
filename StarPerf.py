fcd313055d37c16383e2c54517e6c9fa31d82a36
kangchenghua <chenghuakang@gmail.com>
2026-05-15 02:11:04 +0200

    Update StarPerf.py

--- StarPerf.py
+++ StarPerf.py
@@ -11,8 +11,8 @@ Added implementation of security simulation
 def main():
     print("Starting StarPerf...")
 
-    import src.constellation_generation.by_manual.constellation_information as constellation_information
-    constellation_information.constellation_information("config/manual_constellation_generation_json_file/Example.json")
+    import src.constellation_generation.by_XML.constellation_information as constellation_information
+    constellation_information.constellation_information("config/")
 
     print("Starting XML Constellations Testing...")
     # test the core module functionality of various XML constellations
